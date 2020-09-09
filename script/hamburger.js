27a383bbd175f95dd9b3838f40a164ef324abfdd
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-09-08 22:38:31 -0300

    Finish mobile layout

--- script/hamburger.js
+++ script/hamburger.js
@@ -1,12 +1,17 @@
+
+let navigationLink, main, navDisplay
+window.addEventListener('load', start)
+
+function start() {
+    navigationLink = document.getElementById('header-navigation-mobile');
+    main = document.getElementById('main');
+}
 function handleClickHamburger() {
-    const navigationLinks = document.getElementById('header-navigation-mobile');
-    const menuIcon = document.getElementById('header-menu-icon');
-    const main = document.getElementById('main');
-    if (navigationLinks.style.display === 'none') {
-        navigationLinks.style.display = 'block';
+    if (navigationLink.style.display === 'none' || !navigationLink.style.display) {
+        navigationLink.style.display = 'block';
         main.style.display = 'none';
     } else {
-        navigationLinks.style.display = 'none';
+        navigationLink.style.display = 'none';
         main.style.display = 'block';
     }
 }
