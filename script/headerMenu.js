6691af6c14b152a1ba7a77754e6d110f0bcc29b8
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-09-09 12:39:15 -0300

    Finish desktop header

--- script/headerMenu.js
+++ script/headerMenu.js
@@ -3,10 +3,10 @@ let navigationLink, main, navDisplay
 window.addEventListener('load', start)
 
 function start() {
-    navigationLink = document.getElementById('header-navigation-mobile');
+    navigationLink = document.getElementById('header-navigation');
     main = document.getElementById('main');
 }
-function handleClickHamburger() {
+function handleClickOpenMenu() {
     if (navigationLink.style.display === 'none' || !navigationLink.style.display) {
         navigationLink.style.display = 'block';
         main.style.display = 'none';
