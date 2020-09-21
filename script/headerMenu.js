c02616d3eb3f8e80c26886d36a48b73d7158349b
Beatriz Cavallieri <bfcavallieri@gmail.com>
2020-09-20 22:50:57 -0300

    Hide footer when mobile menu is expanded

--- script/headerMenu.js
+++ script/headerMenu.js
@@ -1,17 +1,21 @@
 
-let navigationLink, main, navDisplay
+let navigation, main, footer
 window.addEventListener('load', start)
 
 function start() {
-    navigationLink = document.getElementById('header-navigation');
-    main = document.getElementById('main');
+    navigation = document.getElementById('header-navigation');
+    main = document.getElementsByTagName('main');
+    footer = document.getElementsByTagName('footer');
 }
+
 function handleClickOpenMenu() {
-    if (navigationLink.style.display === 'none' || !navigationLink.style.display) {
-        navigationLink.style.display = 'block';
-        main.style.display = 'none';
+    if (navigation.style.display === 'none' || !navigation.style.display) {
+        navigation.style.display = 'block';
+        main[0].style.display = 'none';
+        footer[0].style.display = 'none';
     } else {
-        navigationLink.style.display = 'none';
-        main.style.display = 'block';
+        navigation.style.display = 'none';
+        main[0].style.display = 'block';
+        footer[0].style.display = 'block';
     }
 }
